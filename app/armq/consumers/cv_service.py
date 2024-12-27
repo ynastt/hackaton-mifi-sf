25083bef2374b26ac667e9688910ebdb303a9f7d
vladimir.bek <v.bek@integraaal.com>
2024-12-27 16:08:19 +0300

    fix

--- app/armq/consumers/cv_service.py
+++ app/armq/consumers/cv_service.py
@@ -49,6 +49,7 @@ class CVConsumer:
                 message_body_json: dict = json.loads(message.body)
                 try:
                     model_res = self.photo_classifier.get_exhibit_label(message_body_json['photo'])
+                    print(model_res)
                 except Exception as err:
                     logging.error(err)
                     await self.bot.send_message(message_body_json['tg_id'],
@@ -58,6 +59,8 @@ class CVConsumer:
                     return await self.bot.send_message(message_body_json['tg_id'],
                                                        'На вашем фото обнаружен объект живописи, '
                                                        'который идентифицировать не удалось')
+                elif model_res is None or model_res == '':
+                    return await self.bot.send_message(message_body_json['tg_id'], 'Не знаю что это за экспонат')
                 exhibit = await crud_methods.get_exhibit_by_label(model_res)
 
                 exhibit_info = f"""
